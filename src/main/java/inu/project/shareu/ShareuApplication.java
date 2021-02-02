5e43687000d9d5f7a509eba24879e610f6d5edf0
juhwan0815 <juwom0831@naver.com>
2021-02-02 22:27:47 +0900

    shareU 서버 프로젝트 생성

---
+++ src/main/java/inu/project/shareu/ShareuApplication.java
@@ -0,0 +1,13 @@
+package inu.project.shareu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inu/project/shareu/ShareuApplicationTests.java
@@ -0,0 +1,13 @@
+package inu.project.shareu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
